476f0accd889258cf4ea7871a3258aa59798d209
Aidan Farhi <aidanmcfarhi@gmail.com>
2024-11-04 08:47:56 -0500

    added a bunch of styling and other stuff

--- app.go
+++ app.go
@@ -5,6 +5,8 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
 
 	"github.com/AidanFarhi/sitlog/controller"
 	"github.com/AidanFarhi/sitlog/repository"
@@ -17,8 +19,24 @@ type Templates struct {
 }
 
 func NewTemplates() Templates {
+	tmpl := template.New("")
+	err := filepath.Walk("static/views", func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() && filepath.Ext(path) == ".html" {
+			_, err := tmpl.ParseFiles(path)
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	})
+	if err != nil {
+		log.Fatalf("Error parsing templates: %v", err)
+	}
 	return Templates{
-		Templates: template.Must(template.ParseGlob("static/views/*.html")),
+		Templates: tmpl,
 	}
 }
 
@@ -34,6 +52,7 @@ func main() {
 	}
 
 	templates := NewTemplates()
+	fs := http.FileServer(http.Dir("static"))
 
 	eventRepo := repository.NewSimpleEventRepository(db)
 	eventService := service.NewSimpleEventService(eventRepo)
@@ -41,9 +60,16 @@ func main() {
 
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
-		templates.Templates.ExecuteTemplate(w, "index", nil)
+	mux.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodGet {
+			templates.Templates.ExecuteTemplate(w, "index", nil)
+		} else {
+			http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		}
 	})
+
 	mux.HandleFunc("GET /api/v1/events/{childId}/{adultId}", eventController.GetEventsForChild)
 	mux.HandleFunc("POST /api/v1/events/create", eventController.CreateEvent)
 
