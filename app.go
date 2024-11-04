0037abe19ce192d747d93c181f78bae3281084e5
Aidan Farhi <aidanmcfarhi@gmail.com>
2024-11-03 21:51:48 -0500

    added a form

--- app.go
+++ app.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"html/template"
 	"log"
 	"net/http"
 
@@ -10,6 +11,16 @@ import (
 	"github.com/AidanFarhi/sitlog/service"
 	_ "github.com/mattn/go-sqlite3"
 )
+
+type Templates struct {
+	Templates *template.Template
+}
+
+func NewTemplates() Templates {
+	return Templates{
+		Templates: template.Must(template.ParseGlob("static/views/*.html")),
+	}
+}
 
 func main() {
 
@@ -22,12 +33,17 @@ func main() {
 		log.Fatal(err)
 	}
 
+	templates := NewTemplates()
+
 	eventRepo := repository.NewSimpleEventRepository(db)
 	eventService := service.NewSimpleEventService(eventRepo)
 	eventController := controller.NewEventController(eventService)
 
 	mux := http.NewServeMux()
 
+	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		templates.Templates.ExecuteTemplate(w, "index", nil)
+	})
 	mux.HandleFunc("GET /api/v1/events/{childId}/{adultId}", eventController.GetEventsForChild)
 	mux.HandleFunc("POST /api/v1/events/create", eventController.CreateEvent)
 
